609daf8587db5192527b2690924c7a489c9cd2ac
Gizem Demir <92909583+GizemDemir22@users.noreply.github.com>
2021-11-12 09:15:50 +0300

    Revert "Revert "Masa Classıve AdisyonKapat interface eklendi.""
    This reverts commit 0b78c916f637502dc38cba5c3a83ab2e37c05f43.

---
+++ JavaVeJavacilar/Models/Masa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavaVeJavacilar.Models
+{
+   public class Masa:IAdisyonKapat
+    {
+        public Boolean Dolumu { get; set; }
+        List<Siparis> Siparisler { get; set; }
+        decimal IAdisyonKapat.AdisyonKapat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
